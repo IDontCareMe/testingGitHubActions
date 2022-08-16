9a3301cddc923c76b02611618a5814a73c40b0de
Nikolai Voronin <54110187+IDontCareMe@users.noreply.github.com>
2022-08-17 00:50:04 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,4 +3,7 @@
   paragraph.innerHTML = "Проверка-проверка, 1-2-3-4-5!";
   let div = document.createElement('div');
   div.innerHTML = "<strong>Привет, GitHub!</strong>";
+  
+  document.body.append(paragraph);
+  document.body.append(div);
 })()
