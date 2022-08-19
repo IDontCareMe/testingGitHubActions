9c79b310cb2a1aab1427794d18e1bb8abe267ceb
Nikolai Voronin <54110187+IDontCareMe@users.noreply.github.com>
2022-08-19 23:44:06 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 (() => {
   let paragraph = document.createElement('p');
-  paragraph.innerHTML = "Проверка-проверка, 1-2-3-4-5!";
+  paragraph.innerHTML = "one-two-three-testing!, 1-2-3-4-5!";
   let div = document.createElement('div');
   div.innerHTML = "<strong>Привет, GitHub!</strong>";
+  let wrapper = document.querySelector("#wrapper");
   
-  document.body.append(paragraph);
+  //document.body.append(paragraph);
+  wrapper.append(paragraph);
   document.body.append(div);
 })()
